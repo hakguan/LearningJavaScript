00b377628f7d2569658625dddc0137ff920b836e
hakguan <linus.hy@icloud.com>
2019-05-31 13:16:37 +0900

    basic

--- basic_exercise/practices.js
+++ basic_exercise/practices.js
@@ -324,3 +324,67 @@ function restoreArray(arr) {
 console.log(restoreArray(arr1));
 console.log(restoreArray(arr2));
 console.log(restoreArray(arr3));
+
+/* ---------------------------------------- */
+console.log("--------------")
+console.log("5-5. 문자 개수 세기")
+console.log("--------------")
+
+// 주어진 단어(word)에 특정 알파벳(ch)이 몇 번 들어가는지 세어주는 함수
+function countCharacter(word, ch) {
+    var count = 0;
+
+    // 코드를 작성해주세요.
+    for (value of word) {
+        if (value.toUpperCase() === ch.toUpperCase()) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+// 단어 word에 알파벳 'A'가 몇 번 나오는지 세어주는 함수
+function countA(word) {
+    // 코드를 작성해주세요.
+    return countCharacter(word, 'A');
+}
+
+// 테스트 코드
+console.log(countCharacter('AbaCedEA', 'E'));
+console.log(countCharacter('AbaCedEA', 'X'));
+console.log(countA('AbaCedEA'));
+
+/* ---------------------------------------- */
+console.log("--------------")
+console.log("5-7. 데이터 분석하기")
+console.log("--------------")
+
+var birthdayData = [
+    '20010309전소미',
+    '19960828김세정',
+    '19991112최유정',
+    '19960209김청하',
+    '19990719김소혜',
+    '19981216주결경',
+    '19971201정채연',
+    '19991204김도연',
+    '19991204강미나',
+    '19951218임나영',
+    '19990803유연정'
+];
+
+function printMinors(arr) {
+    console.log("미성년자 명단:");
+
+    // 코드를 작성하세요.
+    for (value of birthdayData) {
+        if (Number(value.substring(0, 4)) > 1998) {
+            console.log(value);
+        }
+    }
+    // 이 함수에 리턴값은 없습니다.
+}
+
+// 테스트 코드
+printMinors(birthdayData);
