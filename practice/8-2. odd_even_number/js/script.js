7c90ef0f20e5c611fad2412019ec3b3bc3eddbc1
hakguan <linus.hy@icloud.com>
2019-06-11 21:53:48 +0900

    8-2. Odd Even Number

---
+++ practice/8-2. odd_even_number/js/script.js
@@ -0,0 +1,38 @@
+// $('button').on('click', function () {
+//     $('.card').removeClass('selected');
+
+//     if (event.target.id == 'even-btn') {
+//         $('.card').each(function () {
+//             var cardNum = $(this).text();
+//             if (cardNum % 2 === 0) {
+//                 $(this).addClass('selected');
+//             }
+//         })
+//     } else if (event.target.id == 'odd-btn') {
+//         $('.card').each(function () {
+//             var cardNum = $(this).text();
+//             if (cardNum % 2 === 1) {
+//                 $(this).addClass('selected');
+//             }
+//         })
+//     }
+// });
+
+
+$('#even-btn').on('click', function () {
+    $('.card').removeClass('selected');
+    $('.card').each(function () {
+        if ($(this).text() % 2 == 0) {
+            $(this).addClass('selected');
+        }
+    })
+});
+
+$('#odd-btn').on('click', function () {
+    $('.card').removeClass('selected');
+    $('.card').each(function () {
+        if ($(this).text() % 2 == 1) {
+            $(this).addClass('selected');
+        }
+    })
+});
